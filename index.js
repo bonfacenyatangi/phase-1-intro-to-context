ead062af87ce1e6f620c0d510d3562ff59d91066
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-04-03 22:41:39 +0300

    Payroll intro

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+function createEmployeeRecord(array) {
+    return {
+      firstName: array[0],
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(array => createEmployeeRecord(array));
+  }
+  
+  function createTimeInEvent(employeeRecord, dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    employeeRecord.timeInEvents.push({ type: "TimeIn", date: date, hour: parseInt(hour, 10) });
+    return employeeRecord;
+  }
+  
+  function createTimeOutEvent(employeeRecord, dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    employeeRecord.timeOutEvents.push({ type: "TimeOut", date: date, hour: parseInt(hour, 10) });
+    return employeeRecord;
+  }
+  
+  function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked;
+  }
+  
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const wagesEarned = hoursWorked * employeeRecord.payPerHour;
+    return wagesEarned;
+  }
+  
+  function allWagesFor(employeeRecord) {
+    const dates = employeeRecord.timeInEvents.map(event => event.date);
+    const wages = dates.map(date => wagesEarnedOnDate(employeeRecord, date));
+    const totalWages = wages.reduce((total, wage) => total + wage);
+    return totalWages;
+  }
+  
+  function calculatePayroll(employeeRecords) {
+    const wages = employeeRecords.map(record => allWagesFor(record));
+    const totalWages = wages.reduce((total, wage) => total + wage);
+    return totalWages;
+  }
